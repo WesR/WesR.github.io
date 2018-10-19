351ee64250cdcf3646b650f9a9770f863983e731
wes <wring436@gmail.com>
2018-10-19 02:50:34 -0400

    added email and changed the about work

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,20 @@ window.onload = function () {
             chaffle.init();
         });
     });
+
+    document.getElementById('cpyEmail').addEventListener('click', function (event) {
+        var myEmail = document.getElementById('myEmail');
+        myEmail.focus();
+        myEmail.select();
+        document.execCommand('copy');
+    });
+
+    document.getElementById('myEmail').addEventListener('click', function (event) {
+        var myEmail = document.getElementById('myEmail');
+        document.getElementById("emailForm").reset();
+        myEmail.focus();
+        myEmail.select();
+    });
 };
 
 if (screen.width <= 650) {//Redirect if its a small screen (probably a phone)
