a92879bd7730bcac6dbe71f58e8a36532fad80ee
wes <wring436@gmail.com>
2018-10-19 03:34:51 -0400

    Added a bit of magic to help with email bots

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,17 @@ window.onload = function () {
         });
     });
 
+    addEmail = function () {
+        //Going to add my email to things, this will nock off the weak ... I hope.
+        eml = 'aGVsbG9Ad2V' + 'zcmluZy5jb20=';
+        document.getElementById('myEmail').value = window.atob(eml);
+        document.getElementById('myEmail').placeholder = window.atob(eml);
+    }
+
+    addEmail();
+
     document.getElementById('cpyEmail').addEventListener('click', function (event) {
+        addEmail();
         var myEmail = document.getElementById('myEmail');
         myEmail.focus();
         myEmail.select();
@@ -20,8 +30,8 @@ window.onload = function () {
     });
 
     document.getElementById('myEmail').addEventListener('click', function (event) {
+        addEmail();
         var myEmail = document.getElementById('myEmail');
-        document.getElementById("emailForm").reset();
         myEmail.focus();
         myEmail.select();
     });
