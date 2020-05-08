073f2b319df4f7e82d2a3f30bf73f4d45d16ea6e
BuildTools <wring436@gmail.com>
2020-05-08 01:50:10 -0400

    Trying a new website

--- assets/js/email.js
+++
@@ -1,5 +0,0 @@
-window.onload = function () {
-    //Going to add my email to things, this will nock off the weak ... I hope.
-    eml = 'aGVsbG9Ad2V' + 'zcmluZy5jb20=';
-    document.getElementById('mailLink').href = 'mailto:' + window.atob(eml);
-};
